a224c842f44e15f5b7bd47aac63fa67973e68983
Josep Subils <josep@spellbook.io>
2017-06-27 11:12:07 +0200

    [update] es6 syntax changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express = require('express'),
+const express = require('express'),
   bodyParser = require('body-parser'),
   conf = require('./conf.json'),
   cors = require('cors'),
@@ -9,7 +9,7 @@ var express = require('express'),
   db_gen = require('./server/db_gen.js'),
   db = db_gen(),
   http_req = require('./scaffold/http.js'),
-  socket_req = require('./scaffold/socket.js');
+  socket_req = require('./scaffold/socket.js')
 
 app.use(cors())
 app.use('/public', express.static('public'))
@@ -22,11 +22,11 @@ app.get('/', (req, res) => {
 
 app.listen(conf.http_port, () => {
   console.log(`HTTP: ${conf.http_port} SOCKETS: ${conf.sockets_port}`)
-  http_req(http_gen({ http : app, db : db }));
+  http_req(http_gen({ http : app, db : db }))
 })
 
 io.attach(conf.sockets_port)
 
-io.on('connection', (socket) => {
-  socket_req(socket_gen({ socket : socket, db : db }));
+io.on('connection', socket => {
+  socket_req(socket_gen({ socket : socket, db : db }))
 })
--- scaffold/http.js
+++ scaffold/http.js
@@ -1,4 +1,4 @@
-module.exports = function (api) {
+module.exports = api => {
   api.gen('users', (http, db) => {
     
     db.loadDatabase(err => {
